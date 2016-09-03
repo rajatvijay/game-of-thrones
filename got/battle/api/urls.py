6bd8734634c3bf9eac97fe4102fd8b22b843c100
Rajat Vijay <rajatvijay5@gmail.com>
2016-09-04 02:25:13 +0530

    Changed the endpoint to stats

--- got/battle/api/urls.py
+++ got/battle/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^battles/list$', views.BattleListView.as_view(), name='battle_list'),
     url(r'^battles/(?P<pk>\d+)/$', views.BattleDetailView.as_view(), name='battle_detail'),
     url(r'^battles/count$', views.BattleCountView.as_view(), name='battle_count'),
-    url(r'^battles/stat$', views.StatView.as_view(), name='battle_stat'),
+    url(r'^battles/stats$', views.StatView.as_view(), name='battle_stats'),
 
 ]
