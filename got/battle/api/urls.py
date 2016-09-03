e66ca652337fd4cd02ce009da11b30f743b345e3
Rajat Vijay <rajatvijay5@gmail.com>
2016-09-04 03:47:20 +0530

    Added search functionality by all four methods

--- got/battle/api/urls.py
+++ got/battle/api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^battles/(?P<pk>\d+)/$', views.BattleDetailView.as_view(), name='battle_detail'),
     url(r'^battles/count$', views.BattleCountView.as_view(), name='battle_count'),
     url(r'^battles/stats$', views.StatView.as_view(), name='battle_stats'),
+    url(r'^battles/search$', views.SearchView.as_view(), name='battle_search'),
 
 ]
--- got/battle/api/views.py
+++ got/battle/api/views.py
@@ -1,19 +1,24 @@
-from rest_framework import generics
-from ..models import Battle
+from ..models import Battle, King
 from .serializers import BattleSerializer, StatSerializer
 from got.response import JSONResponse
 from rest_framework.views import APIView
 from ..stats import Stats
 
 
-class BattleListView(generics.ListAPIView):
-    queryset = Battle.objects.all()
-    serializer_class = BattleSerializer
+class BattleListView(APIView):
+    permission_classes = ()
+
+    def get(self, request):
+        batlles = BattleSerializer(instance=Battle.objects.all(), many=True).data
+        return JSONResponse(batlles)
 
 
-class BattleDetailView(generics.RetrieveAPIView):
-    queryset = Battle.objects.all()
-    serializer_class = BattleSerializer
+class BattleDetailView(APIView):
+    permission_classes = ()
+
+    def get(self, request, pk):
+        battle = BattleSerializer(instance=Battle.objects.get(id=pk)).data
+        return JSONResponse(battle)
 
 
 # class BattleDetailView(APIView):
@@ -31,9 +36,40 @@ class BattleCountView(APIView):
         count = Battle.objects.count()
         return JSONResponse(count)
 
+
 class StatView(APIView):
     permission_classes = ()
 
     def get(self, request):
         stat = StatSerializer(instance=Stats()).data
         return JSONResponse(stat)
+
+
+class SearchView(APIView):
+    permission_classes = ()
+
+    def get(self, request):
+        name = request.query_params.get('name', None)
+        type = request.query_params.get('type', None)
+        location = request.query_params.get('location', None)
+        attacker_king = request.query_params.get('attacker_king', None)
+        defender_king = request.query_params.get('defender_king', None)
+        if name:
+            battle = BattleSerializer(instance=Battle.objects.get(name__iexact=name)).data
+            return JSONResponse(battle)
+        elif type:
+            battle = BattleSerializer(instance=Battle.objects.filter(battle_type__iexact=type), many=True).data
+            return JSONResponse(battle)
+        elif location:
+            battle = BattleSerializer(instance=Battle.objects.filter(location__iexact=location), many=True).data
+            return JSONResponse(battle)
+        elif attacker_king:
+            king_object = King.objects.get(name__iexact=attacker_king)
+            battle = BattleSerializer(instance=Battle.objects.filter(attacker_king=king_object), many=True).data
+            return JSONResponse(battle)
+        elif defender_king:
+            king_object = King.objects.get(name__iexact=defender_king)
+            battle = BattleSerializer(instance=Battle.objects.filter(defender_king=king_object), many=True).data
+            return JSONResponse(battle)
+        else:
+            return JSONResponse(name)
