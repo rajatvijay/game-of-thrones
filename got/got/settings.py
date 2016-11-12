d638f2a5aab790bbdbc2ba07df9e011b79e3fdf0
Rajat Vijay <rajatvijay5@gmail.com>
2016-11-13 00:53:41 +0530

    cros

--- got/got/settings.py
+++ got/got/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -40,10 +41,12 @@ INSTALLED_APPS = (
     'battle',
     'django_extensions',
     'rest_framework',
+    'corsheaders'
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
